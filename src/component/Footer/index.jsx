ceceb86599229c73c9b256cbec8d100294ff1aea
ryandi <purbaryandi@gmail.com>
2022-01-31 11:11:35 +0700

    update footer

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -17,11 +17,15 @@ export default function Footer() {
               Follow our Twitter and Discord for more War of Kaos announcements
             </p>
             <a href="https://twitter.com/WarofKaos">
-              <button className="my-1 btn-twitter">Follow Twitter</button>{" "}
+              <button className="my-1 btn-twitter">
+                <span>Follow Twitter</span>
+              </button>{" "}
             </a>
             <br />
             <a href="https://discord.gg/ecEhbGyeZw">
-              <button className="my-1 btn-discord">Follow Discord</button>
+              <button className="my-1 btn-discord">
+                <span>Follow Discord</span>
+              </button>
             </a>
           </div>
         </div>
