9a58b9ee7a4b8d40c11e1660707d4c45b7fb814f
ryandi <purbaryandi@gmail.com>
2022-02-17 10:52:14 +0700

    update js

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,23 @@
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
 $(document).ready(function () {
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll > 300) {
-      $(".navbar").css("background", "black");
-      $(".navbar").css(
-        "box-shadow",
-        "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
-      );
-    } else {
-      $(".navbar").css("background", "none");
-      $(".navbar").css("box-shadow", "none");
-    }
-  });
+  if (screen.width < 1114) {
+    $(".navbar").css("background", "black");
+  } else {
+    $(window).scroll(function () {
+      var scroll = $(window).scrollTop();
+      if (scroll > 300) {
+        $(".navbar").css("background", "black");
+        $(".navbar").css(
+          "box-shadow",
+          "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
+        );
+      } else {
+        $(".navbar").css("background", "none");
+        $(".navbar").css("box-shadow", "none");
+      }
+    });
+  }
+
+  console.log(screen.width);
 });
