483ae849dc27b8db0412d3f845265a82dcb58c6e
ryandi <purbaryandi@gmail.com>
2022-02-24 21:37:28 +0700

    add sniper rarity

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./style.css";
+import Sniper from "../../assets/images/rarity sniper.png";
 
 export default function Footer() {
   return (
     <div class="footer-basic">
       <footer>
+        <a href="https://raritysniper.com">
+          <img
+            src={Sniper}
+            alt="rarity sniper"
+            className="img-fluid rarity-sniper"
+          />
+        </a>
         <div class="social">
           <a
             href="https://discord.gg/k6h6qmM6gU"
