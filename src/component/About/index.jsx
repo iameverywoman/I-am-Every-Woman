3c659bae7853504811731cc96b4617ff36d5ff5b
ryandi <purbaryandi@gmail.com>
2022-02-26 01:05:07 +0700

    update about

--- src/component/About/index.jsx
+++ src/component/About/index.jsx
@@ -15,13 +15,12 @@ export default function About() {
           >
             <h1 className="t-shadow pb-3">About</h1>
             <p className="">
-              Expansion ThugBirdz is on a mission to pay homage to their O.G.
-              feathered homies, the ThugBirdz. The Expansion ThugBirdz
-              collection features a distinct “yellow” background. These birdz
-              are truly unique and each has a different set of traits, making
-              each one-of-a-kind. This flock of ThugBirdz are pecking their way
-              up the ranks in the underworld of the Metaverse, so be on the
-              lookout!
+              Expansion ThugBirdz is an expansion pack of birdz who are thugs.
+              The collection features a distinct “yellow” background. These
+              birdz are truly unique and each has a different set of traits,
+              making each one-of-a-kind. This flock of ThugBirdz are pecking
+              their way up the ranks in the underworld of the Metaverse, so be
+              on the lookout!
             </p>
             <h3 className="t-shadow"> Different Gang, Same Street Cred.</h3>
             <p>
